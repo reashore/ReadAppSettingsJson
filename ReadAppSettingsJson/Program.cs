56987c8b246b6950d7fad8b8e2e95898a4e7a99d
Frank J. Reashore <reashore@telus.net>
2018-08-14 18:32:08 -0700

    Initial commit

--- ReadAppSettingsJson/Program.cs
+++ ReadAppSettingsJson/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace ReadAppSettingsJson
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        // https://blog.bitscry.com/2017/05/30/appsettings-json-in-net-core-console-app/
+
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+
+            string configurationValue = configuration["parameter1"];
+            //string configurationValue = configuration.GetConnectionString("connection1");
+
+            Console.WriteLine($"configurationValue = {configurationValue}");
+            Console.WriteLine("Done");
+            Console.ReadKey();
         }
     }
 }
